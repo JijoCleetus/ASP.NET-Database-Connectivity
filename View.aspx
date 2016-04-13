5f01595e38a740ea62b7ed951e4289b06ae56a65
Jijo Cleetus <jijocleetus@gmail.com>
2016-04-13 16:43:20 +0530

    Added files via upload

---
+++ View.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="View.aspx.cs" Inherits="DatabaseConnectivityinASP.View" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" href="master.css" />
+    </head>
+<body>
+
+</body>
+</html>
---
+++ View.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DatabaseConnectivityinASP
+{
+    public partial class View : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=.\\EAUDIT;Integrated Security=SSPI;Initial Catalog=angularDB";
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand cmd;
+            string sql = "SELECT * FROM Company";
+            SqlDataReader dataReader;
+            try
+            {
+                con.Open();
+                cmd = new SqlCommand(sql, con);
+                dataReader = cmd.ExecuteReader();
+                Response.Write("<table border='1'>");
+                while (dataReader.Read())
+                {
+                    Response.Write("<tr>");
+                    Response.Write("<td>" + dataReader.GetValue(0) + "</td><td>" + dataReader.GetValue(1) + "</td><td>" + dataReader.GetValue(2)+"</td>");
+                    Response.Write("</tr>");
+                }
+                Response.Write("</table>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.ToString());
+            }
+        }
+    }
+}
